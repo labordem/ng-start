c3be57712cc0c5b3635aff90088283ae16893f54
miaborde <38043788+mIaborde@users.noreply.github.com>
2021-01-11 00:07:09 +0100

    fix: import

--- src/app/core/guards/user.guard.spec.ts
+++ src/app/core/guards/user.guard.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { UserGuard } from '../user.guard';
+import { UserGuard } from './user.guard';
 
 describe('UserGuard', () => {
   let guard: UserGuard;
